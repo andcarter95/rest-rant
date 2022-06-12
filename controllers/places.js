bdae9a0d3ca28b0ec6c6d2f304a5c939a009c60c
andcarter95 <and.carter95@gmail.com>
2022-06-12 10:45:26 -0500

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -21,6 +21,20 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
 
 // GET /places
 router.get('/', (req, res) => {
