ae88a3d1c0f12fd2b1adf22555cc848738edc81c
andcarter95 <and.carter95@gmail.com>
2022-06-13 23:05:40 -0500

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -49,6 +49,19 @@ router.delete('/:id', (req, res) => {
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
 // GET /places
 router.get('/', (req, res) => {
       res.render('places/index', { places })
